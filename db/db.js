9e21687bae7fd3316f6caa2756cc01f4917b406b
prashantgoswami <soultaker399@gmail.com>
2025-06-14 00:23:47 +0530

    updated postgres

--- db/db.js
+++ db/db.js
@@ -18,7 +18,7 @@ async function authenticateDB() {
         await db.authenticate();
         console.log('Connection has been established successfully.');
     } catch (error) {
-        console.error('Unable to connect to the database:', error);
+        console.error('Unable to connect to the database:', error.message);
     }
 }
 
